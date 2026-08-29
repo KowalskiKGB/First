48c630ace2f13b1e338667bc074bb562939fcc79
Rafael Casa <rafael.ap.10.rf@gmail.com>
2026-08-29 13:18:07 -0300

    feat: enable android passkey association

--- frontend/android/app/src/main/java/com/kowalskikgb/first/MainActivity.java
+++ frontend/android/app/src/main/java/com/kowalskikgb/first/MainActivity.java
@@ -1,5 +1,19 @@
 package com.kowalskikgb.first;
 
+import android.os.Bundle;
+import androidx.webkit.WebSettingsCompat;
+import androidx.webkit.WebViewFeature;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getBridge() != null && WebViewFeature.isFeatureSupported(WebViewFeature.WEB_AUTHENTICATION)) {
+            WebSettingsCompat.setWebAuthenticationSupport(
+                getBridge().getWebView().getSettings(),
+                WebSettingsCompat.WEB_AUTHENTICATION_SUPPORT_FOR_APP
+            );
+        }
+    }
+}
